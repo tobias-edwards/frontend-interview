d9b28361377531fa0449fd690f29443c1cb12801
Tobias Edwards <tobias_r_edwards@hotmail.co.uk>
2025-01-21 00:16:34 +0000

    Update header styles for desktop

--- COMMENTS.md
+++ COMMENTS.md
@@ -1,1 +1,3 @@
 <!-- Project Comments Go Here -->
+
+- Nit: inconsistent casing for class names, migrated to camel-case in most places
--- src/Header.tsx
+++ src/Header.tsx
@@ -4,7 +4,7 @@ import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <div className={styles.Header}>
+    <div className={styles.header}>
       <LogoSvg className={styles.logo} />
       <h1>Application Portal</h1>
     </div>
